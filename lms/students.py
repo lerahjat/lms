95b61cdda4aa78fc1631d3eb28658dbe4c1c2fe3
lerahja <lerafox2590@gmail.com>
2021-09-29 23:12:30 +0300

    students.py

--- lms/students.py
+++ lms/students.py
@@ -11,71 +11,97 @@ TEST_STUDENTS = [
     ['Andy', 'H', 'more_mail@mail.com', 'sexteen', 'Brighton', 'M']
 ]
 
-def add_student():
-    student = {}
-    for field in student_fields:
-        student[field] = input('Enter {}\t'.format(field))
-        if field == 'age':
-            try:
-                int(student['age'])
-            except:
-                student['age'] = input('Enter age as number\t')
-    STUDENTS.append(student)
 
-def calculate_avg_age():
-    try:
-        total_age = 0
+class Student:
+
+    def __init__(self, first_name, last_name, email, age, address, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.address = address
+        self.gender = gender
+
+    def add_student(self):
+        student = {}
+        for field in student_fields:
+            student[field] = input('Enter {}\t'.format(field))
+            if field == 'age':
+                try:
+                    int(student['age'])
+                except:
+                    student['age'] = input('Enter age as number\t')
+        STUDENTS.append(student)
+
+    def print_student(self, student):
+        for field in student:
+            print(field.title().replace("_", " "), ":", '\t', '\t', student[field])
+
+
+class Group:
+
+    def __init__(self):
+        pass
+
+    def load_students(self):
+        for test_student in TEST_STUDENTS:
+            STUDENTS.append(dict(zip(student_fields, test_student)))
+
+    def dump_studens(self):
+        with open('data/student_data.json', 'w') as file:
+            json.dump(STUDENTS, file)
+
+    def dump_csv(self):
+        with open('data/student_data.csv', 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=student_fields)
+            writer.writeheader()
+            for student in STUDENTS:
+                writer.writerow(student)
+
+    def load_csv(self, file_path='data/student_data.csv'):
+        with open(file_path, newline='') as file:
+            reader = csv.reader(file)  # load_csv
+            batch_data = list(reader)
+            student = []
+            for keys in batch_data:
+                STUDENTS = {student_fields[i]: keys[i] for i in range(len(student_fields))}
+                student.append(STUDENTS)
+            dict_student = {'student_fields': student}
+        print(dict_student)
+
+    def load_from_json(self, file_path='data/student_data.json'):
+        with open(file_path, 'r') as read_file:
+            STUDENTS.extend(json.load(read_file))
+
+    def calculate_avg_age(self):
+        try:
+            total_age = 0
+            for student in STUDENTS:
+                total_age += int(student['age'])
+            avgerage_age = total_age / len(STUDENTS)
+            print('Average age is {}'.format(avgerage_age))
+        except ValueError:
+            print('Cannot calculate average age')
+        except Exception as e:
+            print(str(e))
+
+    def print_strudents_list(self):
         for student in STUDENTS:
-            total_age += int(student['age'])
-        avgerage_age = total_age / len(STUDENTS)
-        print('Average age is {}'.format(avgerage_age))
-    except ValueError:
-        print('Cannot calculate average age')
-    except Exception as e:
-        print(str(e))
-
-def print_student(student):
-    for field in student:
-        print(field.title().replace("_", " "),":", '\t','\t',student[field])
-
-def print_strudents_list():
-    for student in STUDENTS:
-        print(print_student(student),"------------------------")
+            print(Student.print_student(student), "------------------------")
 
 
-
-def load_students():
-    for test_student in TEST_STUDENTS:
-        STUDENTS.append(dict(zip(student_fields, test_student)))
-
-def dump_studens():
-    with open('lms/data/student_data.json', 'w') as file:
-        json.dump(STUDENTS, file)
-
-def dump_csv():
-    with open('lms/data/student_data.csv', 'w') as file:
-        writer = csv.DictWriter(file, fieldnames=student_fields)
-        writer.writeheader()
-        for student in STUDENTS:
-            writer.writerow(student)
-
-def load_csv(file_path='student_data.csv'):
-    pass
-
-
-def load_from_json(file_path='student_data.json'):
-    with open(file_path, 'r') as read_file:
-        STUDENTS.extend(json.load(read_file))
+group = Group()
+student = Student('Mary', 'D', 'mail@mail.com', '19', 'Huston', 'F')
 
 ACTIONS = {
-    'add': add_student,
-    'avg_age': calculate_avg_age,
-    'load': load_students,
-    'print': print_strudents_list,
-    'dump': dump_studens,
-    'dump_csv': dump_csv,
-    'load_json': load_from_json,
-    'load_csv': load_csv
+    'add': student.add_student,
+    'avg_age': group.calculate_avg_age,
+    'load': group.load_students,
+    'print': group.print_strudents_list,
+    'dump': group.dump_studens,
+    'dump_csv': group.dump_csv,
+    'load_json': group.load_from_json,
+    'load_csv': group.load_csv
 }
 
 if __name__ == '__main__':
@@ -85,3 +111,9 @@ if __name__ == '__main__':
             ACTIONS.get(action)()
         else:
             break
+
+
+
+
+
+
